Turns out using the wrong normalization stats for cifar10

cifar10/train_cifar10.py
@@ -102,7 +102,7 @@ def torch_loader(data_path, size):
     # Data loading code
     traindir = os.path.join(data_path, 'train')
     valdir = os.path.join(data_path, 'test')
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(mean=[0.4914 , 0.48216, 0.44653], std=[0.24703, 0.24349, 0.26159])
 
     train_tfms = transforms.Compose([
     #   transforms.RandomResizedCrop(size),
@@ -309,7 +309,7 @@ def main():
 
     print('Finished!')
     
-    if args.use_tta::
+    if args.use_tta:
         log_preds,y = learner.TTA()
         preds = np.mean(np.exp(log_preds),0)
         acc = accuracy(torch.FloatTensor(preds),torch.LongTensor(y))