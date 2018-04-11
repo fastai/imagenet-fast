1fa3f0f8cf4698786a89a91db81dd15d3f76344d
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-04-11 08:42:52 -0400

    Adding script to train cifar10. Also adding TTA

--- cifar10/models/cifar10/__init__.py
+++ cifar10/models/cifar10/__init__.py
@@ -1,4 +1,5 @@
 from .preact_resnet import *
 from .resnext import *
 from .senet import *
+from .clr_resnet import *
 
--- cifar10/train_cifar10.py
+++ cifar10/train_cifar10.py
@@ -21,36 +21,46 @@ import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import models
+import models.cifar10 as cifar10models
 from distributed import DistributedDataParallel as DDP
 
+# print(models.cifar10.__dict__)
 model_names = sorted(name for name in models.__dict__
                      if name.islower() and not name.startswith("__")
                      and callable(models.__dict__[name]))
+
+cifar10_names = sorted(name for name in cifar10models.__dict__
+                     if name.islower() and not name.startswith("__")
+                     and callable(cifar10models.__dict__[name]))
+
+model_names = cifar10_names + model_names
+
+
 # print(model_names)
 
 # Example usage: python run_fastai.py /home/paperspace/ILSVRC/Data/CLS-LOC/ -a resnext_50_32x4d --epochs 1 -j 4 -b 64 --fp16
 
-parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser = argparse.ArgumentParser(description='PyTorch Cifar10 Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
 parser.add_argument('--save-dir', type=str, default=Path.home()/'imagenet_training',
                     help='Directory to save logs and models.')
-parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet56',
                     choices=model_names,
                     help='model architecture: ' +
                     ' | '.join(model_names) +
-                    ' (default: resnet18)')
-parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    ' (default: resnet56)')
+parser.add_argument('-j', '--workers', default=7, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
+parser.add_argument('--epochs', default=1, type=int, metavar='N',
                     help='number of total epochs to run')
-parser.add_argument('--cycle-len', default=1, type=float, metavar='N',
+parser.add_argument('--cycle-len', default=95, type=float, metavar='N',
                     help='Length of cycle to run')
 # parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
 #                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=256, type=int,
+parser.add_argument('-b', '--batch-size', default=512, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.8, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
@@ -63,13 +73,12 @@ parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
 #                     help='evaluate model on validation set')
 parser.add_argument('--pretrained', dest='pretrained', action='store_true', help='use pre-trained model')
 parser.add_argument('--fp16', action='store_true', help='Run model fp16 mode.')
-parser.add_argument('--use-tta', action='store_true', help='Validate model with TTA at the end of traiing.')
-parser.add_argument('--train-128', action='store_true', help='Train model on 128. TODO: allow custom epochs and LR')
-parser.add_argument('--sz',       default=224, type=int, help='Size of transformed image.')
+parser.add_argument('--use-tta', default=True, type=bool, help='Validate model with TTA at the end of traiing.')
+parser.add_argument('--sz',       default=32, type=int, help='Size of transformed image.')
 # parser.add_argument('--decay-int', default=30, type=int, help='Decay LR by 10 every decay-int epochs')
-parser.add_argument('--use-clr', type=str, 
+parser.add_argument('--use-clr', default='10,13.68,0.95,0.85', type=str, 
                     help='div,pct,max_mom,min_mom. Pass in a string delimited by commas. Ex: "20,2,0.95,0.85"')
-parser.add_argument('--loss-scale', type=float, default=1,
+parser.add_argument('--loss-scale', type=float, default=128,
                     help='Loss scaling, positive power of 2 values can improve fp16 convergence.')
 parser.add_argument('--prof', dest='prof', action='store_true', help='Only run a few iters for profiling.')
 
@@ -84,45 +93,55 @@ parser.add_argument('--rank', default=0, type=int,
                     help='Used for multi-process training. Can either be manually set ' +
                     'or automatically set by using \'python -m multiproc\'.')
 
+class TorchModelData(ModelData):
+    def __init__(self, path, trn_dl, val_dl, aug_dl=None):
+        super().__init__(path, trn_dl, val_dl)
+        self.aug_dl = aug_dl
+
 def torch_loader(data_path, size):
     # Data loading code
     traindir = os.path.join(data_path, 'train')
-    valdir = os.path.join(data_path, 'val')
+    valdir = os.path.join(data_path, 'test')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-    train_dataset = datasets.ImageFolder(
-        traindir,
-        transforms.Compose([
-            transforms.RandomResizedCrop(size),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ]))
-
+    train_tfms = transforms.Compose([
+    #   transforms.RandomResizedCrop(size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    train_dataset = datasets.ImageFolder(traindir, train_tfms)
     train_sampler = (torch.utils.data.distributed.DistributedSampler(train_dataset)
                      if args.distributed else None)
-
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
         num_workers=args.workers, pin_memory=True, sampler=train_sampler)
 
+    val_tfms = transforms.Compose([
+    #   transforms.Resize(int(size*1.14)),
+    #   transforms.CenterCrop(size),
+        transforms.ToTensor(),
+        normalize,
+    ])
     val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(int(size*1.14)),
-            transforms.CenterCrop(size),
-            transforms.ToTensor(),
-            normalize,
-        ])),
+        datasets.ImageFolder(valdir, val_tfms),
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
-    
+
+
+    aug_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, train_tfms),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
     train_loader = DataPrefetcher(train_loader)
     val_loader = DataPrefetcher(val_loader)
+    aug_loader = DataPrefetcher(aug_loader)
     if args.prof:
         train_loader.stop_after = 200
         val_loader.stop_after = 0
 
-    data = ModelData(data_path, train_loader, val_loader)
+    data = TorchModelData(data_path, train_loader, val_loader, aug_loader)
     return data, train_sampler
 
 
@@ -163,8 +182,7 @@ class DataPrefetcher():
             yield input, target
             if type(self.stop_after) is int and (count > self.stop_after):
                 break
-
-
+                
 def top5(output, target):
     """Computes the precision@k for the specified values of k"""
     top5 = 5
@@ -229,12 +247,13 @@ def update_model_dir(learner, base_dir):
     learner.models_path = f'{base_dir}/models'
     os.makedirs(learner.models_path, exist_ok=True)
     
+
 # This is important for speed
 cudnn.benchmark = True
-global args
-args = parser.parse_args()
-print('Running script with args:', args)
-
+global arg
+args = parser.parse_args(args_input); args
+if args.cycle_len > 1: args.cycle_len = int(args.cycle_len)
+    
 def main():
     args.distributed = args.world_size > 1
     args.gpu = 0
@@ -248,52 +267,34 @@ def main():
 
     if args.fp16:
         assert torch.backends.cudnn.enabled, "fp16 mode requires cudnn backend to be enabled."
-
+        
     # create model
+    model = cifar10models.__dict__[args.arch] if args.arch in cifar10_names else models.__dict__[args.arch] 
     if args.pretrained:
         print("=> using pre-trained model '{}'".format(args.arch))
-        model = models.__dict__[args.arch](pretrained=True)
+        model = model(pretrained=True)
     else:
         print("=> creating model '{}'".format(args.arch))
-        model = models.__dict__[args.arch]()
-
+        model = model()
+        
     model = model.cuda()
     if args.distributed:
         model = DDP(model)
         
-    if args.train_128:
-        data, train_sampler = torch_loader(f'{args.data}-160', 128)
-    else:
-        data, train_sampler = torch_loader(args.data, args.sz)
+    data, train_sampler = torch_loader(args.data, args.sz)
 
     learner = Learner.from_model_data(model, data)
+    # learner.crit = F.nll_loss
     learner.crit = F.cross_entropy
-    learner.metrics = [accuracy, top5]
+    learner.metrics = [accuracy]
     if args.fp16: learner.half()
-        
+
     if args.prof:
         args.epochs = 1
-        args.cycle_len=1
+        args.cycle_len=.01
     if args.use_clr:
         args.use_clr = tuple(map(float, args.use_clr.split(',')))
-    
-    # 128x128
-    if args.train_128:
-        save_dir = args.save_dir+'/128'
-        update_model_dir(learner, save_dir)
-        sargs = save_args('first_run_128', save_dir)
-        learner.fit(args.lr,args.epochs, cycle_len=args.cycle_len,
-                    train_sampler=train_sampler,
-                    wds=args.weight_decay,
-                    use_clr_beta=args.use_clr,
-                    loss_scale=args.loss_scale,
-                    **sargs
-                )
-        save_sched(learner.sched, save_dir)
-        data, train_sampler = torch_loader(args.data, args.sz)
-        learner.set_data(data)
-
-
+        
     # Full size
     update_model_dir(learner, args.save_dir)
     sargs = save_args('first_run', args.save_dir)
@@ -306,10 +307,13 @@ def main():
                )
     save_sched(learner.sched, args.save_dir)
 
-    if args.use_tta:
-        print('TTA is disabled for now. Need to add aug_dl to enable this')
-        # print(accuracy(*learner.TTA()))
-        
     print('Finished!')
     
-main()
+    if args.use_tta::
+        log_preds,y = learner.TTA()
+        preds = np.mean(np.exp(log_preds),0)
+        acc = accuracy(torch.FloatTensor(preds),torch.LongTensor(y))
+        print('TTA acc:', acc)
+
+        with open(args.save_dir+'/tta_accuracy.txt', "a", 1) as f:
+            f.write(time.strftime("%Y-%m-%dT%H:%M:%S")+f"\tTTA accuracty: {acc}\n")
