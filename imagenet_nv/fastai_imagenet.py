41cfcee9ca029fcb93e44b8677279468aea1570b
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-04-12 01:55:06 -0700

    Lighting and jitter

--- imagenet_nv/fastai_imagenet.py
+++ imagenet_nv/fastai_imagenet.py
@@ -89,6 +89,35 @@ class TorchModelData(ModelData):
         super().__init__(path, trn_dl, val_dl)
         self.aug_dl = aug_dl
 
+
+__imagenet_pca = {
+    'eigval': torch.Tensor([0.2175, 0.0188, 0.0045]),
+    'eigvec': torch.Tensor([
+        [-0.5675,  0.7192,  0.4009],
+        [-0.5808, -0.0045, -0.8140],
+        [-0.5836, -0.6948,  0.4203],
+    ])
+}
+
+class Lighting(object):
+    """Lighting noise(AlexNet - style PCA - based noise)"""
+
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = eigval
+        self.eigvec = eigvec
+
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone()\
+            .mul(alpha.view(1, 3).expand(3, 3))\
+            .mul(self.eigval.view(1, 3).expand(3, 3))\
+            .sum(1).squeeze()
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+
 def torch_loader(data_path, size):
     # Data loading code
     traindir = os.path.join(data_path, 'train')
@@ -98,7 +127,9 @@ def torch_loader(data_path, size):
     train_tfms = transforms.Compose([
         transforms.RandomResizedCrop(size),
         transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(.3,.3,.3),
         transforms.ToTensor(),
+        Lighting(0.1, __imagenet_pca['eigval'], __imagenet_pca['eigvec']),
         normalize,
     ])
     train_dataset = datasets.ImageFolder(traindir, train_tfms)
