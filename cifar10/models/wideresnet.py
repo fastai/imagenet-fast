602566c647e423b19eb0f98fdf385df9240e5c11
Jeremy Howard <j@howard.fm>
2018-04-14 14:41:46 -0700

    new block for wrn

--- cifar10/models/wideresnet.py
+++ cifar10/models/wideresnet.py
@@ -11,7 +11,7 @@ def bn_relu_conv(ni, nf, ks, stride):
     return nn.Sequential(nn.BatchNorm2d(ni), nn.ReLU(inplace=True), conv_2d(ni, nf, ks, stride))
 def noop(x): return x
 
-class BasicBlock2(nn.Module):
+class BasicBlock(nn.Module):
     def __init__(self, ni, nf, stride, drop_p=0.0):
         super().__init__()
         self.bn = nn.BatchNorm2d(ni)
@@ -29,27 +29,6 @@ class BasicBlock2(nn.Module):
         return x.add_(r)
 
 
-class BasicBlock(nn.Module):
-    def __init__(self, ni, nf, stride, drop_p=0.0):
-        super().__init__()
-        self.bn1 = nn.BatchNorm2d(ni)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv1 = conv_2d(ni, nf, 3, stride)
-        self.bn2 = nn.BatchNorm2d(nf)
-        self.conv2 = conv_2d(nf, nf, 3, 1)
-        self.drop = nn.Dropout(drop_p, inplace=True) if drop_p else None
-        self.same = (ni == nf)
-        if not self.same: self.conv_shortcut = conv_2d(ni, nf, 1, stride)
-
-    def forward(self, x):
-        if self.same: out = self.relu(self.bn1(x))
-        else:         x   = self.relu(self.bn1(x))
-        out = self.relu(self.bn2(self.conv1(out if self.same else x)))
-        if self.drop: out = self.drop(out)
-        out = self.conv2(out)
-        return out.add_(x if self.same else self.conv_shortcut(x))
-
-
 def _make_group(N, ni, nf, block, stride, drop_p):
     return [block(ni if i == 0 else nf, nf, stride if i == 0 else 1, drop_p) for i in range(N)]
 
@@ -61,10 +40,9 @@ class WideResNet(nn.Module):
 
         layers = [conv_2d(3, n_channels[0], 3, 1)]  # conv1
         for i in range(num_groups):
-            layers += _make_group(N, n_channels[i], n_channels[i+1], BasicBlock2, (1 if i==0 else 2), drop_p)
+            layers += _make_group(N, n_channels[i], n_channels[i+1], BasicBlock, (1 if i==0 else 2), drop_p)
 
-        layers += [nn.BatchNorm2d(n_channels[-1]), nn.ReLU(inplace=True),
-                   nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(n_channels[-1], num_classes)]
+        layers += [nn.AdaptiveAvgPool2d(1), bn_relu_conv(n_channels[-1], num_classes, 1, 1), Flatten()]
         self.features = nn.Sequential(*layers)
 
     def forward(self, x): return self.features(x)
