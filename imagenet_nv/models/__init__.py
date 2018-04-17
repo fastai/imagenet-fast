4acce59a6e7a4b61267ab98ea12b8f19f9ae0a08
Jeremy Howard <j@fast.ai>
2018-04-17 17:54:22 +0000

    fix models

--- imagenet_nv/models/__init__.py
+++ imagenet_nv/models/__init__.py
@@ -1,5 +1,5 @@
 from torchvision.models.resnet import *
-from .resnet2 import *
+from .resnet import *
 from .resnet3 import *
 from .dpn import *
 from .darknet import *
@@ -7,11 +7,6 @@ from .se_resnet import *
 from .se_resnext import *
 from .pre_resnet import *
 from .resnext import *
-from .resnext_101_32x4d import *
-from .resnext_50_32x4d import *
-from .resnext_101_64x4d import *
-from .wrn_50_2f import *
-from .nasnet import *
 from .inceptionresnetv2 import *
 from .inceptionv4 import *
 
