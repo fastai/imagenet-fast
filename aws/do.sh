a3941a69ec44c7e2f7954e409202d95103f82b7f
Jeremy Howard <j@fast.ai>
2018-04-19 22:51:00 +0000

    shell scripts

--- aws/do.sh
+++ aws/do.sh
@@ -1,11 +1,16 @@
-#python run_script.py --launch-method demand -iname bnf_10 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a bnfinal_resnet50 --lr 0.1'"
+python run_script.py --launch-method demand -iname rn50_10 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a resnet50 --lr 0.1'"
+python run_script.py --launch-method demand -iname rn50_13 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a resnet50 --lr 0.13'"
+python run_script.py --launch-method demand -iname rn50_20 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a resnet50 --lr 0.20'"
+python run_script.py --launch-method demand -iname rn50_30 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a resnet50 --lr 0.30'"
+python run_script.py --launch-method demand -iname rn50_40 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a resnet50 --lr 0.40'"
 
-#python run_script.py --launch-method demand -iname fa4_10 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a fa4_resnet50 --lr 0.1'"
-#python run_script.py --launch-method demand -iname fa4_13 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a fa4_resnet50 --lr 0.13'"
-python run_script.py --launch-method demand -iname fa4_30 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a fa4_resnet50 --lr 0.30'"
-python run_script.py --launch-method demand -iname fa4_40 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a fa4_resnet50 --lr 0.40'"
+python run_script.py --launch-method demand -iname bnz_20 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a bnz_resnet50 --lr 0.20'"
+python run_script.py --launch-method demand -iname bnz_30 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a bnz_resnet50 --lr 0.30'"
+python run_script.py --launch-method demand -iname bnz_40 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a bnz_resnet50 --lr 0.40'"
 
-#python run_script.py --launch-method demand -iname fa5_20 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a fa5_resnet50 --lr 0.2'"
-#python run_script.py --launch-method demand -iname fa5_07 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a fa5_resnet50 --lr 0.07'"
-#python run_script.py --launch-method demand -iname fa5_13 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a fa5_resnet50 --lr 0.13'"
+python run_script.py --launch-method demand -iname w15_13 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a w15_resnet50 --lr 0.13'"
+python run_script.py --launch-method demand -iname w125_13 --run-script upload_scripts/train2.sh -sargs "-sh -sargs '-a w125_resnet50 --lr 0.13'"
+
+python run_script.py --launch-method demand -iname rn50_13_wd35 --run-script upload_scripts/train2.sh -sargs \
+	"-sh -sargs '--wd 3e-5 -a resnet50 --lr 0.13'"
 
