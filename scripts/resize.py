8c4ca44b7a8490cff0d107990ffe4add4244b369
nofreewill42 <jonavan01@gmail.com>
2021-03-05 22:10:21 +0100

    Remove not used argument
    No usage of argument p in resizes and resize_img functions.

--- scripts/resize.py
+++ scripts/resize.py
@@ -12,7 +12,7 @@ DEST = Path.home()/'data/imagenet-sz'
 #szs = (int(128*1.25), int(256*1.25))
 szs = (int(160*1.25),)
 
-def resize_img(p, im, fn, sz):
+def resize_img(im, fn, sz):
     w,h = im.size
     ratio = min(h/sz,w/sz)
     im = im.resize((int(w/ratio), int(h/ratio)), resample=Image.BICUBIC)
@@ -21,14 +21,14 @@ def resize_img(p, im, fn, sz):
     new_fn.parent.mkdir(exist_ok=True)
     im.save(new_fn)
 
-def resizes(p, fn):
+def resizes(fn):
     im = Image.open(fn)
-    for sz in szs: resize_img(p, im, fn, sz)
+    for sz in szs: resize_img(im, fn, sz)
 
 def resize_imgs(p):
     files = p.glob('*/*.JPEG')
     #list(map(partial(resizes, p), files))
-    with ProcessPoolExecutor(cpus) as e: e.map(partial(resizes, p), files)
+    with ProcessPoolExecutor(cpus) as e: e.map(resizes, files)
 
 
 for sz in szs:
